d9535eca6de06cf7a74a8fdab4377725d084cb8d
ankit-hsc <luckyandlucy@gmail.com>
2019-08-08 17:19:39 +0530

    three

--- components/accounts/src/main/java/io/pivotal/pal/tracker/accounts/RegistrationService.java
+++ components/accounts/src/main/java/io/pivotal/pal/tracker/accounts/RegistrationService.java
@@ -3,12 +3,14 @@ package io.pivotal.pal.tracker.accounts;
 import io.pivotal.pal.tracker.accounts.data.AccountDataGateway;
 import io.pivotal.pal.tracker.users.data.UserDataGateway;
 import io.pivotal.pal.tracker.users.data.UserRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
-
+Logger logger= LoggerFactory.getLogger(RegistrationService.class);
     private final UserDataGateway userDataGateway;
     private final AccountDataGateway accountDataGateway;
 
@@ -19,6 +21,7 @@ public class RegistrationService {
 
     @Transactional
     public UserRecord createUserWithAccount(String name) {
+        logger.info("inside createUserWithAccount() method");
         UserRecord user = userDataGateway.create(name);
         accountDataGateway.create(user.id, String.format("%s's account", name));
         return user;
